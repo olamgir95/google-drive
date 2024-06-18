d66526a98dce84a05ef008139463c02750db1024
Olamgir <ollomurodovolamgir233@gmail.com>
2024-06-18 14:58:15 +0900

    feat: sidebar and userbox developed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import React, { ReactNode } from "react";
 import Navbar from "./../../components/shared/navbar";
+import Sidebar from "@/components/shared/sidebar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
       <Navbar />
+      <Sidebar />
       {children}
     </div>
   );
